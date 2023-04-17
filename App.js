83c8d6e7f2f5d1820558b7362cfdc812f7cc8108
Sarah Kuss <sarah.kuss33@gmail.com>
2023-04-17 13:04:13 -0400

    created jokes app using react native

--- App.js
+++ App.js
@@ -1,20 +1,33 @@
+import { useEffect, useState, Fragment } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View, TouchableOpacity } from 'react-native';
+import { styles } from './src/styles';
 
 export default function App() {
+  const [jokes, setJokes] = useState();
+  const [index, setIndex] = useState(0);
+  useEffect(() => {
+    fetch('https://api.sampleapis.com/jokes/goodJokes')
+      .then(response => response.json())
+      .then(setJokes);
+  }, []);
+  const getNextJoke = () => {
+    if(index < jokes.length - 1) setIndex(index + 1)
+    else setIndex(0)
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    {(!jokes)
+      ? <Text style={styles.text}>Loading jokes...</Text>
+      : <Fragment>
+        <Text style={styles.text}>{jokes[index].setup}</Text>
+        <Text style={styles.punchline}>{jokes[index].punchline}</Text>
+      <TouchableOpacity style={styles.bigButton} onPress={getNextJoke}>
+        <Text style={styles.buttonText}>Tell me another</Text>
+      </TouchableOpacity>
+    </Fragment>}
       <StatusBar style="auto" />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/styles.js
@@ -0,0 +1,31 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#262262',
+    alignItems: 'center',
+    justifyContent: 'space-around',
+  },
+
+  text: {
+    color: 'whitesmoke',
+    fontSize: 48,
+    width: '90%',
+    textAlign: 'center',
+  },
+  punchline: {
+    textAlign: 'center',
+    color: '#FFBD02',
+    fontSize: 32,
+  },
+  bigButton: {
+    backgroundColor: 'FF216E',
+    padding: 24,
+    borderRadius: 6,
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 30,
+  },
+});
